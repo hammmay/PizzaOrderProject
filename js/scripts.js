5947ebdfdbed047a79c534e95c91818eba40825d
Laura Hamilton <laurahamilton9@gmail.com>
2017-06-16 11:03:58 -0700

    Add outputs for new objects for html and add show function for html id pizza-order-summary

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 //business logic
 
-function Ticket (argument1, argument2){
+function Pizza (argument1, argument2){
    this.size = argument1;
    this.toppings = argument2;
    this.price = 10;
 }
+
 
 
 // user interface logic
@@ -14,17 +15,18 @@ $(document).ready(function(){
 
     var checkedSize = $("input[name='size-of-pizza']:checked").val();
     var arrayOfSelectedToppings = [];
-    $("input:checkbox[name=toppings]:checked").each(function(){
+    $("input:checkbox[name=toppings-on-pizza]:checked").each(function(){
       var selectedToppings = $(this).val();
       arrayOfSelectedToppings.push(selectedToppings);
-
-    var myPizza = new Pizza(checkedSize, arrayOfSelectedToppings);
-
-
-// NOTE: just appends and shows the list of toppings into the html element pizza-order-summary  $('#pizza-order-summary').append(selectedToppings + "<br>");
     });
 
-// Only works with the pizza-order-summary append function $("#pizza-order-summary").show();
+    var myPizza = new Pizza (checkedSize, arrayOfSelectedToppings);
+
+    $(".size-output").append(myPizza.size);
+    $(".toppings-output").append(myPizza.toppings);
+
+    $("#pizza-order-summary").show();
+
 
   });
 });
