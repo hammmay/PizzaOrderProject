fc2d41e92ef1e95f3ab9143184b6b1f87eec41ec
Laura Hamilton <laurahamilton9@gmail.com>
2017-06-16 11:11:37 -0700

    Add function to UI to erase previous inputs each time the submit button is activated

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,10 @@ $(document).ready(function(){
   $("form#all-pizza-options").submit(function(event){
     event.preventDefault();
 
+    $("#pizza-order-summary span").each(function() {
+      $(this).empty();
+    });
+
     var checkedSize = $("input[name='size-of-pizza']:checked").val();
     var arrayOfSelectedToppings = [];
     $("input:checkbox[name=toppings-on-pizza]:checked").each(function(){
